379fc2881b7b5b1b4d1babbc7e8b85276b1084d7
imada kakeru <imada@fun.bio.keio.ac.jp>
2024-12-05 14:19:24 +0900

    before add remote repository

--- make_dataSet_for_model.py
+++ make_dataSet_for_model.py
@@ -1,6 +1,18 @@
 import torch
 
 
-def devide_dataSet():
+def devide_dataSet(result, correct, ratio):
+    train = []
+    validation = []
+    test = []
+    return train, validation, test
+
+def make_data_loader(result_data, correct_data):
+
     return
 
+
+def make_data_loader_for_model(result, correct, ratio):
+    train, validation, test = devide_dataSet(result=result, correct=correct, ratio=ratio)
+
+    train = make_data_loader()
